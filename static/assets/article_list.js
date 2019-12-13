be3a9cd22e08c3841a3ffa862f897c212b33b259
catherine <catherine@qq.com>
2019-12-13 19:31:56 +0800

    .

--- static/assets/article_list.js
+++ static/assets/article_list.js
@@ -2,19 +2,51 @@
 $.ajax({
     type: 'get',
     url: 'http://localhost:8080/api/v1/admin/article/query',
-    success: function(response) {
+    success: function (response) {
         // console.log(response);
         // 拼接文章列表字符串
-        var html=template('listTpl',{
-            data:response.data.data
+        var html = template('listTpl', {
+            data: response.data.data
         });
         // console.log(html);
         $('#listBox').html(html);
 
+        var pageArr = [];
+        for (var i = 1; i <= response.data.totalPage; i++) {
+            pageArr.push(i)
+        };
+        // console.log(pageArr);
+        var page = template('pageTpl', {
+            data: pageArr
+        });
+        $('#pageBox').html(page);
 
-        // var page=1
 
-        
     }
 });
 
+// 封装函数，实现分页
+function changePage(page) {
+    console.log(page+"---------------");
+    
+    // 发送请求，获取文章列表数据
+    $.ajax({
+        type: 'get',
+        url: 'http://localhost:8080/api/v1/admin/article/query',
+        data: {
+            page: page
+        },
+        success: function (response) {
+            console.log(response);
+            response.data.data.currentPage=page;
+            response.data.data.pages=response.data.totalPage;
+            console.log(page);
+            var html = template('listTpl', {
+                data: response.data.data
+            });
+            $('#listBox').html(html);
+            console.log(response);
+
+        }
+    })
+}
