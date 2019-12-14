42d4f3735d452d390aa0372b01ec4e3db63acfe7
jiangming <hong@qq.com>
2019-12-14 11:21:34 +0800

    中午提交

--- static/assets/login.js
+++ static/assets/login.js
@@ -7,7 +7,7 @@ $('#loginForm').on('submit',function(){
      url:'http://localhost:8080/api/v1/admin/user/login',
      data:data,
      success:function(result){
-        //  console.log(result);
+         console.log(result);
         if (result.msg==='登录成功'){
             window.localStorage.setItem('token',result.token);
             location.href='/admin/index.html';
